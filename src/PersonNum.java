d80d485d137410ebf08238aa0e1184b61fd17fc9
HyungjuJang <dacapolife87@gmail.com>
2020-06-18 15:26:57 +0900

    Initial commit

---
+++ src/PersonNum.java
@@ -0,0 +1,19 @@
+public class PersonNum {
+    public static void main(String[] args) {
+        String personNum = "8711111234567";
+        String prefixNum = personNum.substring(0,6);
+        String divisionNum = String.valueOf(personNum.charAt(6));
+        String birthDay = "";
+        switch (divisionNum){
+            case "1":
+            case "2":
+                birthDay = "19"+prefixNum;
+                break;
+            case "3":
+            case "4":
+                birthDay = "20"+prefixNum;
+                break;
+        }
+        System.out.println(birthDay);
+    }
+}

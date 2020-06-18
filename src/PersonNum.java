d7c90b34a813ec36bb82416aa0378691df837f7f
HyungjuJang <dacapolife87@gmail.com>
2020-06-18 15:28:33 +0900

    if / switch case

--- src/PersonNum.java
+++ src/PersonNum.java
@@ -4,16 +4,21 @@ public class PersonNum {
         String prefixNum = personNum.substring(0,6);
         String divisionNum = String.valueOf(personNum.charAt(6));
         String birthDay = "";
-        switch (divisionNum){
-            case "1":
-            case "2":
-                birthDay = "19"+prefixNum;
-                break;
-            case "3":
-            case "4":
-                birthDay = "20"+prefixNum;
-                break;
+        if(divisionNum.equals("1") || divisionNum.equals("2") ){
+            birthDay = "19"+prefixNum;
+        }else{
+            birthDay = "20"+prefixNum;
         }
+//        switch (divisionNum){
+//            case "1":
+//            case "2":
+//                birthDay = "19"+prefixNum;
+//                break;
+//            case "3":
+//            case "4":
+//                birthDay = "20"+prefixNum;
+//                break;
+//        }
         System.out.println(birthDay);
     }
 }
